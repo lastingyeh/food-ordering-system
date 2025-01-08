bcaf0abdf22aae564247fe084395ae60d43b906b
ChrisYeh <chrisyeh@cathayholdings.com.tw>
2025-01-08 08:57:38 +0800

    fix infrastructure.kafka

--- infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
@@ -10,7 +10,6 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PreDestroy;
-
 import java.io.Serializable;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
@@ -25,14 +24,6 @@ public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordB
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @PreDestroy
-    public void close() {
-        if (kafkaTemplate != null) {
-            log.info("Closing kafka producer!");
-            kafkaTemplate.destroy();
-        }
-    }
-
     @Override
     public void send(String topicName, K key, V message, BiConsumer<SendResult<K, V>, Throwable> callback) {
         log.info("Sending message={} to topic={}", message, topicName);
@@ -41,8 +32,16 @@ public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordB
             kafkaResultFuture.whenComplete(callback);
         } catch (KafkaException e) {
             log.error("Error on kafka producer with key: {}, message: {} and exception: {}", key, message,
-                    e.getMessage());
+                    e.getMessage(), e);
             throw new KafkaProducerException("Error on kafka producer with key: " + key + " and message: " + message);
+        }
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            log.info("Closing kafka producer!");
+            kafkaTemplate.destroy();
         }
     }
 }
