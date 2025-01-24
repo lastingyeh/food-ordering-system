8a17451b43e6a69c72b0d09273a6991a5f88473d
Chris Yeh <lastingyeh@gmail.com>
2025-01-24 17:33:54 +0800

    refactoring restaurant-service, customer-service

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantApprovalRequestHelper.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantApprovalRequestHelper.java
@@ -9,7 +9,6 @@ import com.food.ordering.system.restaurant.service.domain.exception.RestaurantNo
 import com.food.ordering.system.restaurant.service.domain.mapper.RestaurantDataMapper;
 import com.food.ordering.system.restaurant.service.domain.outbox.model.OrderOutboxMessage;
 import com.food.ordering.system.restaurant.service.domain.outbox.scheduler.OrderOutboxHelper;
-import com.food.ordering.system.restaurant.service.domain.ports.output.message.publisher.RestaurantApprovalResponseMessagePublisher;
 import com.food.ordering.system.restaurant.service.domain.ports.output.repository.OrderApprovalRepository;
 import com.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -30,28 +29,23 @@ public class RestaurantApprovalRequestHelper {
     private final RestaurantRepository restaurantRepository;
     private final OrderApprovalRepository orderApprovalRepository;
     private final OrderOutboxHelper orderOutboxHelper;
-    private final RestaurantApprovalResponseMessagePublisher restaurantApprovalResponseMessagePublisher;
-
 
 
     public RestaurantApprovalRequestHelper(RestaurantDomainService restaurantDomainService,
                                            RestaurantDataMapper restaurantDataMapper,
                                            RestaurantRepository restaurantRepository,
                                            OrderApprovalRepository orderApprovalRepository,
-                                           OrderOutboxHelper orderOutboxHelper,
-                                           RestaurantApprovalResponseMessagePublisher
-                                                   restaurantApprovalResponseMessagePublisher) {
+                                           OrderOutboxHelper orderOutboxHelper) {
         this.restaurantDomainService = restaurantDomainService;
         this.restaurantDataMapper = restaurantDataMapper;
         this.restaurantRepository = restaurantRepository;
         this.orderApprovalRepository = orderApprovalRepository;
         this.orderOutboxHelper = orderOutboxHelper;
-        this.restaurantApprovalResponseMessagePublisher = restaurantApprovalResponseMessagePublisher;
     }
 
     @Transactional
     public void persistOrderApproval(RestaurantApprovalRequest restaurantApprovalRequest) {
-        if (publishIfOutboxMessageProcessed(restaurantApprovalRequest)) {
+        if (isOutboxMessageProcessed(restaurantApprovalRequest)) {
             log.info("An outbox message with saga id: {} already saved to database!",
                     restaurantApprovalRequest.getSagaId());
             return;
@@ -88,24 +82,19 @@ public class RestaurantApprovalRequestHelper {
         restaurant.setActive(restaurantEntity.isActive());
         restaurant.getOrderDetail().getProducts().forEach(product ->
                 restaurantEntity.getOrderDetail().getProducts().forEach(p -> {
-                    if (p.getId().equals(product.getId())) {
-                        product.updateWithConfirmedNamePriceAndAvailability(p.getName(), p.getPrice(), p.isAvailable());
-                    }
-                }));
+            if (p.getId().equals(product.getId())) {
+                product.updateWithConfirmedNamePriceAndAvailability(p.getName(), p.getPrice(), p.isAvailable());
+            }
+        }));
         restaurant.getOrderDetail().setId(new OrderId(UUID.fromString(restaurantApprovalRequest.getOrderId())));
 
         return restaurant;
     }
 
-    private boolean publishIfOutboxMessageProcessed(RestaurantApprovalRequest restaurantApprovalRequest) {
+    private boolean isOutboxMessageProcessed(RestaurantApprovalRequest restaurantApprovalRequest) {
         Optional<OrderOutboxMessage> orderOutboxMessage =
                 orderOutboxHelper.getCompletedOrderOutboxMessageBySagaIdAndOutboxStatus(UUID
                         .fromString(restaurantApprovalRequest.getSagaId()), OutboxStatus.COMPLETED);
-        if (orderOutboxMessage.isPresent()) {
-            restaurantApprovalResponseMessagePublisher.publish(orderOutboxMessage.get(),
-                    orderOutboxHelper::updateOutboxStatus);
-            return true;
-        }
-        return false;
+        return orderOutboxMessage.isPresent();
     }
 }
--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/mapper/RestaurantDataMapper.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/mapper/RestaurantDataMapper.java
@@ -18,16 +18,16 @@ import java.util.stream.Collectors;
 @Component
 public class RestaurantDataMapper {
     public Restaurant restaurantApprovalRequestToRestaurant(RestaurantApprovalRequest
-                                                                             restaurantApprovalRequest) {
+                                                                    restaurantApprovalRequest) {
         return Restaurant.builder()
                 .restaurantId(new RestaurantId(UUID.fromString(restaurantApprovalRequest.getRestaurantId())))
                 .orderDetail(OrderDetail.builder()
                         .orderId(new OrderId(UUID.fromString(restaurantApprovalRequest.getOrderId())))
                         .products(restaurantApprovalRequest.getProducts().stream().map(
-                                product -> Product.builder()
-                                        .productId(product.getId())
-                                        .quantity(product.getQuantity())
-                                        .build())
+                                        product -> Product.builder()
+                                                .productId(product.getId())
+                                                .quantity(product.getQuantity())
+                                                .build())
                                 .collect(Collectors.toList()))
                         .totalAmount(new Money(restaurantApprovalRequest.getPrice()))
                         .orderStatus(OrderStatus.valueOf(restaurantApprovalRequest.getRestaurantOrderStatus().name()))
