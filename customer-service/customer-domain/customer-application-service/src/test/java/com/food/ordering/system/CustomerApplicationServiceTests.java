2b5ca6a41141516fbcd99aa19344aa010a31d0dd
Samson Zheng <samsonzheng@cathayholdings.com.tw>
2025-03-19 11:44:51 +0800

    Remove redundant code for mockito

--- customer-service/customer-domain/customer-application-service/src/test/java/com/food/ordering/system/CustomerApplicationServiceTests.java
+++ customer-service/customer-domain/customer-application-service/src/test/java/com/food/ordering/system/CustomerApplicationServiceTests.java
@@ -9,22 +9,14 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.UUID;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest(classes = CustomerApplicationServiceTestConfig.class)
-//@AutoConfigureStubRunner(ids = {"com.example:http-server-dsl:+:stubs:6565"})
-//@DirtiesContext
 public class CustomerApplicationServiceTests {
 
-    @Autowired
-    CustomerApplicationService customerApplicationService;
+    CustomerApplicationService customerApplicationService = Mockito.mock(CustomerApplicationService.class);
 
     @BeforeAll
     public void init() {
@@ -36,7 +28,7 @@ public class CustomerApplicationServiceTests {
     }
 
     @Test
-    void createUser() {
+    public void createUser() {
         // given
 
         // when
